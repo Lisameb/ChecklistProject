1345ee5bf5dd4238250279a20aca29d9641180e1
Julia K <kurashvili@mail.hs-ulm.de>
2021-12-13 23:40:39 +0100

    added comments to StartProject
    now it's easier to look at windows not integrated into the app flow yet. @Author: Julia

--- src/de/thu/project/main/StartProject.java
+++ src/de/thu/project/main/StartProject.java
@@ -5,6 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import view.ChangeChecklistView;
+import view.ChangeTemplateView;
+import view.ItemView;
 import view.LoginView;
 
 public class StartProject {
@@ -14,8 +17,31 @@ public class StartProject {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					
+					/*
+					 * For login and checklist creation GUI (true start of application):
+					 */
+					
 					LoginView window = new LoginView();
 					window.getFrame().setVisible(true);
+					
+					/*
+					 * For checklist managing GUI (not fully implemented):
+					 */
+					
+//					ChangeChecklistView window = new ChangeChecklistView();
+//					window.setVisible(true);
+					
+					/*
+					 * For template managing GUI (fully functional, but leads
+					 * to add new item into global list of items GUI which isn't
+					 * finished yet):
+					 */
+					
+//					ChangeTemplateView window = new ChangeTemplateView();
+//					window.setVisible(true);
+					
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
